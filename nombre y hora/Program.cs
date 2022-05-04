3efa3890aba5abae975625ae68ca9d54cb6025a6
marcelo boyanosky <marceloboyanosky@gmail.com>
2022-05-04 14:15:49 -0300

    Agregar archivos de proyecto.

---
+++ nombre y hora/Program.cs
@@ -0,0 +1,7 @@
+﻿//ejercicio 1
+Console.WriteLine("Ingrese su nombre");
+ string nombre = Console.ReadLine();
+Console.WriteLine("el nombre ingresado es: "+nombre);
+//ejercicio 2
+DateTime hora = DateTime.Now;
+Console.WriteLine("la hora actual es: "+hora);
